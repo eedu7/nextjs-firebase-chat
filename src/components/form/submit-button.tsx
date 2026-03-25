a7408f3302d5c0c6a14206a8acc800b0762e502d
Mueed Ahmad <mueedahmad067@gmail.com>
2026-03-25 20:38:51 +0500

    feat: added the login form

--- src/components/form/submit-button.tsx
+++ src/components/form/submit-button.tsx
@@ -1,22 +1,25 @@
 import { useFormContext } from "@/context/form";
 import { Button } from "@/components/ui/button";
 import { Spinner } from "../ui/spinner";
+import { Field } from "@/components/ui/field";
 
 export const SubmitButton = ({
     label,
     isPending = false,
 }: {
     label: string;
-    isPending: boolean;
+    isPending?: boolean;
 }): React.JSX.Element => {
     const form = useFormContext();
     return (
-        <form.Subscribe selector={(state) => state.isSubmitting}>
-            {(isSubmitting) => (
-                <Button type="submit" disabled={isSubmitting || isPending}>
-                    {isSubmitting || isPending ? <Spinner /> : label}
-                </Button>
-            )}
-        </form.Subscribe>
+        <Field orientation="horizontal">
+            <form.Subscribe selector={(state) => state.isSubmitting}>
+                {(isSubmitting) => (
+                    <Button type="submit" disabled={isSubmitting || isPending}>
+                        {isSubmitting || isPending ? <Spinner /> : label}
+                    </Button>
+                )}
+            </form.Subscribe>
+        </Field>
     );
 };
--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -1,3 +1,53 @@
+"use client";
+import { useAppForm } from "@/hooks/form";
+import { FieldGroup, FieldSet } from "@/components/ui/field";
+
 export const LoginForm = () => {
-    return <form></form>;
+    const form = useAppForm({
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+    return (
+        <form
+            onSubmit={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                form.handleSubmit();
+            }}
+        >
+            <FieldGroup>
+                <FieldSet>
+                    <FieldGroup>
+                        <form.AppField
+                            name="email"
+                            children={(field) => (
+                                <field.TextField
+                                    label="Email"
+                                    name="email"
+                                    autoComplete="email"
+                                    type="email"
+                                    required
+                                />
+                            )}
+                        />
+                        <form.AppField
+                            name="email"
+                            children={(field) => (
+                                <field.PasswordField
+                                    label="Password"
+                                    autoComplete="password"
+                                    required
+                                />
+                            )}
+                        />
+                    </FieldGroup>
+                </FieldSet>
+                <form.AppForm>
+                    <form.SubmitButton label="Login" />
+                </form.AppForm>
+            </FieldGroup>
+        </form>
+    );
 };
